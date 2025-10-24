e9e71fc302eaed70cf0c4175c7ef999a21f83286
Sigmanificient <edhyjox@gmail.com>
2025-10-24 04:21:18 +0200

    Add a guard to rename similar licenses

--- gen-metadata.py
+++ gen-metadata.py
@@ -9,6 +9,17 @@ import sys
 from operator import itemgetter
 
 
+LICENSE_RENAMES = {
+    "CC0 Public Domain": "CC 1.0",
+    "GNU GPLv3": "GPLv3",
+    "GNU General Public License v3.0": "GPLv3",
+    "GPL-3": "GPLv3",
+    "MIT license": "MIT",
+    "MIT license - https://www.mit.edu/~amini/LICENSE.md": "MIT",
+    "MIT, I prefer credit (and money) to none, but I won't sue": "MIT",
+}
+
+
 def parse_themes(paths):
     cp = subprocess.run(['kitten', '__parse_theme_metadata__'],
                         input='\n'.join(paths).encode('utf-8'), capture_output=True)
@@ -24,6 +35,9 @@ def main():
     files = glob.glob('themes/*.conf')
     parsed = parse_themes(files)
     for theme, td in zip(files, parsed):
+        license_name = td.get('license')
+        if license_name is not None:
+            td['license'] = LICENSE_RENAMES.get(license_name, license_name)
         td['file'] = theme
         if td['name'] in seen:
             raise SystemExit(
